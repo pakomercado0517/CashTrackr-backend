cf048ab8d46e6a9a07464f172e8407f38b2909af
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-02-14 18:47:07 -0600

    fixing some rules on controllers

--- src/emails/AuthEmails.ts
+++ src/emails/AuthEmails.ts
@@ -1,40 +1,40 @@
-import { transport } from "../config/nodemailer"
+import { transport } from '../config/nodemailer';
+const { FRONTEND_URL } = process.env;
 
 type EmailType = {
-  name: string,
-  email: string, 
-  token: string
-}
+  name: string;
+  email: string;
+  token: string;
+};
 
 export class AuthEmail {
   static sendConfirmationEmail = async (user: EmailType) => {
     const email = await transport.sendMail({
       from: 'CashTrackr <admin@cashtrackr.com>',
       to: user.email,
-      subject: "CashTrackr - Confirma tu cuenta",
+      subject: 'CashTrackr - Confirma tu cuenta',
       html: `
       <p>Hola ${user.name}. Has creado tu cuenta en CashTrackr, ya está casi lista.</p>
       <p>Visita el siguiente enlace:</p>
-      <a href="#">Confirma tu cuenta</a>
+      <a href=${FRONTEND_URL}/auth/confirm-acount>Confirma tu cuenta</a>
       <p>e ingresa el código: <b>${user.token}</b></p>
-      `
-    })
-    console.log('email enviado con éxito', email.messageId)
-  }
+      `,
+    });
+    console.log('email enviado con éxito', email.messageId);
+  };
 
   static sendPasswordResetToken = async (user: EmailType) => {
     const email = await transport.sendMail({
       from: 'CashTrackr <admin@cashtrackr.com>',
       to: user.email,
-      subject: "CashTrackr - Reestablece tu Password",
+      subject: 'CashTrackr - Reestablece tu Password',
       html: `
       <p>Hola ${user.name}. Has solicitado reestablecer tu password.</p>
       <p>Visita el siguiente enlace:</p>
-      <a href="#">Reestablecer Password</a>
+      <a href=${FRONTEND_URL}/auth/new-password>Reestablecer Password</a>
       <p>e ingresa el código: <b>${user.token}</b></p>
-      `
-    })
-    console.log('email enviado con éxito', email.messageId)
-  }
+      `,
+    });
+    console.log('email enviado con éxito', email.messageId);
+  };
 }
-
